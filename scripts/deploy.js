2eaa42de0bebe78b63a2aeadf5fee1aa272ad5c1
Robert <cmcorporations.adm@gmail.com>
2022-01-05 20:00:00 -0400

    Add max supply to dployer

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ const deploy = async () => {
   console.log('Deploying contract with account: ', deployer.address);
 
   const CryptoCities = await ethers.getContractFactory('CryptoCities');
-  const deployed = await CryptoCities.deploy();
+  const deployed = await CryptoCities.deploy(10);
 
   console.log('The contract is deployed at: ', deployed.address);
 };
